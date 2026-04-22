f8589f5c74ac8d4c4d8a79e047e69d9623382d3a
Yordanos Fentahun <yordanosfentahundata@gmail.com>
2026-04-22 18:38:31 +0300

    Change motion import from 'motion/react' to 'framer-motion'

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,4 +1,4 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import { Code2, Database, Brain, TrendingUp } from "lucide-react";
 import { Card } from "./ui/card";
 
