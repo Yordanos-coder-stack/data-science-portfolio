c34c306e2337c590fa219d0d3a3f14122e159d46
Yordanos Fentahun <yordanosfentahundata@gmail.com>
2026-05-04 15:17:09 +0300

    Fix typo in highlights for Experience component

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -27,7 +27,7 @@ const education = [
     period: "2023 - present",
     description: "Focused on machine learning, data analysis, and big data technologies. Completed coursework in statistical modeling, data mining, and deep learning.",
     gpa: "expected",
-    highlights: [" BLUE NILE MACHINE INTELLIGENCE LAB(BNMIL) community member(Mentor Team Leader "]
+    highlights: [" Blue Nile Mchine Intelligence Lab (BNMIL) community member(Mentor Team Leader "]
     
   }
 ];
