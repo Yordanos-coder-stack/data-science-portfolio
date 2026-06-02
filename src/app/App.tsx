9927b549cc2ce8c356924008f9fc611330bec198
Yordanos Fentahun <yordanosfentahundata@gmail.com>
2026-06-02 00:50:24 -0700

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,7 +10,7 @@ import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white overflow-x-hidden">
       <Navigation />
       <main id="home">
         <Hero />
